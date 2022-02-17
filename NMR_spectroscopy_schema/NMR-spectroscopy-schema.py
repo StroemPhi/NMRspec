4005c09cfb67d1fdec2ec51be506750e90c080a6
Philip Strömert <Philip.Stroemert@tib.eu>
2022-02-17 21:57:42 +0100

    no inline slots on nmrAssay

--- NMR_spectroscopy_schema/NMR-spectroscopy-schema.py
+++ NMR_spectroscopy_schema/NMR-spectroscopy-schema.py
@@ -1,5 +1,5 @@
 # Auto generated from NMR-spectroscopy-schema.yaml by pythongen.py version: 0.9.0
-# Generation date: 2022-02-17T11:08:16
+# Generation date: 2022-02-17T21:40:31
 # Schema: NMR-spectroscopy-schema
 #
 # id: https://git.tib.eu/lab-linked-scientific-knowledge/nmr-research-data-semantification/-/blob/main/nmr_assay_schema.yaml
@@ -33,7 +33,6 @@ dataclasses._init_fn = dataclasses_init_fn_with_kwargs
 
 # Namespaces
 NMRSPEC = CurieNamespace('NMRspec', 'https://nfdi4chem/nmr/data/')
-NMRMANUFACTURERS = CurieNamespace('NmrManufacturers', 'http://example.org/UNKNOWN/NmrManufacturers/')
 BFO = CurieNamespace('bfo', 'http://purl.obolibrary.org/obo/BFO_')
 CHEBI = CurieNamespace('chebi', 'http://purl.obolibrary.org/obo/CHEBI_')
 CHEMINF = CurieNamespace('cheminf', 'http://purl.obolibrary.org/obo/CHEMINF_')
@@ -48,6 +47,7 @@ NMRCV = CurieNamespace('nmrCV', 'http://nmrML.org/nmrCV#')
 OBI = CurieNamespace('obi', 'http://purl.obolibrary.org/obo/OBI_')
 PATO = CurieNamespace('pato', 'http://purl.obolibrary.org/obo/PATO_')
 PROV = CurieNamespace('prov', 'http://www.w3.org/ns/prov#')
+PUBCHEM = CurieNamespace('pubchem', 'https://pubchem.com/')
 RO = CurieNamespace('ro', 'http://purl.obolibrary.org/obo/RO_')
 SDO = CurieNamespace('sdo', 'https://schema.org/')
 DEFAULT_ = NMRSPEC
@@ -56,7 +56,15 @@ DEFAULT_ = NMRSPEC
 # Types
 
 # Class references
-class NmrBufferId(extended_str):
+class NmrSampleId(extended_str):
+    pass
+
+
+class NmrSolventId(extended_str):
+    pass
+
+
+class NmrSpectrometerId(extended_str):
     pass
 
 
@@ -104,9 +112,9 @@ class PulsedNmrAssay(YAMLRoot):
     class_model_uri: ClassVar[URIRef] = NMRSPEC.PulsedNmrAssay
 
     sample: Union[dict, "NmrSample"] = None
-    buffer: Union[dict, "NmrBuffer"] = None
+    solvent: Union[dict, "NmrSolvent"] = None
+    spectrometer: Union[dict, "NmrSpectrometer"] = None
     type: Union[str, "NmrAssayType"] = None
-    spectrometer: Optional[Union[dict, "NmrSpectrometer"]] = None
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
         if self._is_empty(self.sample):
@@ -114,18 +122,20 @@ class PulsedNmrAssay(YAMLRoot):
         if not isinstance(self.sample, NmrSample):
             self.sample = NmrSample(**as_dict(self.sample))
 
-        if self._is_empty(self.buffer):
-            self.MissingRequiredField("buffer")
-        if not isinstance(self.buffer, NmrBuffer):
-            self.buffer = NmrBuffer(**as_dict(self.buffer))
+        if self._is_empty(self.solvent):
+            self.MissingRequiredField("solvent")
+        if not isinstance(self.solvent, NmrSolvent):
+            self.solvent = NmrSolvent(**as_dict(self.solvent))
+
+        if self._is_empty(self.spectrometer):
+            self.MissingRequiredField("spectrometer")
+        if not isinstance(self.spectrometer, NmrSpectrometer):
+            self.spectrometer = NmrSpectrometer(**as_dict(self.spectrometer))
 
         if self._is_empty(self.type):
             self.MissingRequiredField("type")
         if not isinstance(self.type, NmrAssayType):
             self.type = NmrAssayType(self.type)
-
-        if self.spectrometer is not None and not isinstance(self.spectrometer, NmrSpectrometer):
-            self.spectrometer = NmrSpectrometer(**as_dict(self.spectrometer))
 
         super().__post_init__(**kwargs)
 
@@ -139,11 +149,17 @@ class NmrSample(YAMLRoot):
     class_name: ClassVar[str] = "NmrSample"
     class_model_uri: ClassVar[URIRef] = NMRSPEC.NmrSample
 
+    id: Union[str, NmrSampleId] = None
     name: Optional[str] = None
     local_id: Optional[str] = None
     formula: Optional[str] = None
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        if self._is_empty(self.id):
+            self.MissingRequiredField("id")
+        if not isinstance(self.id, NmrSampleId):
+            self.id = NmrSampleId(self.id)
+
         if self.name is not None and not isinstance(self.name, str):
             self.name = str(self.name)
 
@@ -157,23 +173,23 @@ class NmrSample(YAMLRoot):
 
 
 @dataclass
-class NmrBuffer(YAMLRoot):
+class NmrSolvent(YAMLRoot):
     _inherited_slots: ClassVar[List[str]] = []
 
-    class_class_uri: ClassVar[URIRef] = NMRSPEC.NmrBuffer
-    class_class_curie: ClassVar[str] = "NMRspec:NmrBuffer"
-    class_name: ClassVar[str] = "NmrBuffer"
-    class_model_uri: ClassVar[URIRef] = NMRSPEC.NmrBuffer
+    class_class_uri: ClassVar[URIRef] = NMRSPEC.NmrSolvent
+    class_class_curie: ClassVar[str] = "NMRspec:NmrSolvent"
+    class_name: ClassVar[str] = "NmrSolvent"
+    class_model_uri: ClassVar[URIRef] = NMRSPEC.NmrSolvent
 
-    id: Union[str, NmrBufferId] = None
+    id: Union[str, NmrSolventId] = None
     formula: Optional[str] = None
     name: Optional[str] = None
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
         if self._is_empty(self.id):
             self.MissingRequiredField("id")
-        if not isinstance(self.id, NmrBufferId):
-            self.id = NmrBufferId(self.id)
+        if not isinstance(self.id, NmrSolventId):
+            self.id = NmrSolventId(self.id)
 
         if self.formula is not None and not isinstance(self.formula, str):
             self.formula = str(self.formula)
@@ -185,25 +201,7 @@ class NmrBuffer(YAMLRoot):
 
 
 @dataclass
-class Instrument(YAMLRoot):
-    _inherited_slots: ClassVar[List[str]] = []
-
-    class_class_uri: ClassVar[URIRef] = NMRSPEC.Instrument
-    class_class_curie: ClassVar[str] = "NMRspec:Instrument"
-    class_name: ClassVar[str] = "Instrument"
-    class_model_uri: ClassVar[URIRef] = NMRSPEC.Instrument
-
-    manufacturered_by: Optional[Union[dict, "Manufacturer"]] = None
-
-    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        if self.manufacturered_by is not None and not isinstance(self.manufacturered_by, Manufacturer):
-            self.manufacturered_by = Manufacturer()
-
-        super().__post_init__(**kwargs)
-
-
-@dataclass
-class NmrSpectrometer(Instrument):
+class NmrSpectrometer(YAMLRoot):
     _inherited_slots: ClassVar[List[str]] = []
 
     class_class_uri: ClassVar[URIRef] = NMRSPEC.NmrSpectrometer
@@ -211,54 +209,41 @@ class NmrSpectrometer(Instrument):
     class_name: ClassVar[str] = "NmrSpectrometer"
     class_model_uri: ClassVar[URIRef] = NMRSPEC.NmrSpectrometer
 
-    magnet: Optional[str] = None
-    probe: Optional[str] = None
-    sample_tube: Optional[str] = None
+    id: Union[str, NmrSpectrometerId] = None
+    type: str = None
     manufacturered_by: Optional[Union[dict, "NmrManufacturer"]] = None
-    type: Optional[str] = None
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        if self.magnet is not None and not isinstance(self.magnet, str):
-            self.magnet = str(self.magnet)
+        if self._is_empty(self.id):
+            self.MissingRequiredField("id")
+        if not isinstance(self.id, NmrSpectrometerId):
+            self.id = NmrSpectrometerId(self.id)
 
-        if self.probe is not None and not isinstance(self.probe, str):
-            self.probe = str(self.probe)
-
-        if self.sample_tube is not None and not isinstance(self.sample_tube, str):
-            self.sample_tube = str(self.sample_tube)
+        if self._is_empty(self.type):
+            self.MissingRequiredField("type")
+        if not isinstance(self.type, str):
+            self.type = str(self.type)
 
         if self.manufacturered_by is not None and not isinstance(self.manufacturered_by, NmrManufacturer):
             self.manufacturered_by = NmrManufacturer(**as_dict(self.manufacturered_by))
 
-        if self.type is not None and not isinstance(self.type, str):
-            self.type = str(self.type)
-
         super().__post_init__(**kwargs)
 
 
-class Manufacturer(YAMLRoot):
-    _inherited_slots: ClassVar[List[str]] = []
-
-    class_class_uri: ClassVar[URIRef] = NMRSPEC.Manufacturer
-    class_class_curie: ClassVar[str] = "NMRspec:Manufacturer"
-    class_name: ClassVar[str] = "Manufacturer"
-    class_model_uri: ClassVar[URIRef] = NMRSPEC.Manufacturer
-
-
 @dataclass
-class NmrManufacturer(Manufacturer):
+class NmrManufacturer(YAMLRoot):
     _inherited_slots: ClassVar[List[str]] = []
 
-    class_class_uri: ClassVar[URIRef] = NMRSPEC.NmrManufacturer
-    class_class_curie: ClassVar[str] = "NMRspec:NmrManufacturer"
+    class_class_uri: ClassVar[URIRef] = OBI["0000835"]
+    class_class_curie: ClassVar[str] = "obi:0000835"
     class_name: ClassVar[str] = "NmrManufacturer"
     class_model_uri: ClassVar[URIRef] = NMRSPEC.NmrManufacturer
 
-    type: Optional[str] = None
+    name: Optional[Union[str, "NmrManufacturers"]] = None
 
     def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
-        if self.type is not None and not isinstance(self.type, str):
-            self.type = str(self.type)
+        if self.name is not None and not isinstance(self.name, NmrManufacturers):
+            self.name = NmrManufacturers(self.name)
 
         super().__post_init__(**kwargs)
 
@@ -379,6 +364,23 @@ class NmrManufacturers(EnumDefinitionImpl):
                 PermissibleValue(text="Siemens AG") )
         setattr(cls, "Spinlock SRL",
                 PermissibleValue(text="Spinlock SRL") )
+
+class Dimension(EnumDefinitionImpl):
+
+    _defn = EnumDefinition(
+        name="Dimension",
+    )
+
+    @classmethod
+    def _addvals(cls):
+        setattr(cls, "1D",
+                PermissibleValue(text="1D") )
+        setattr(cls, "2D",
+                PermissibleValue(text="2D") )
+        setattr(cls, "3D",
+                PermissibleValue(text="3D") )
+        setattr(cls, "4D",
+                PermissibleValue(text="4D") )
 
 # Slots
 
