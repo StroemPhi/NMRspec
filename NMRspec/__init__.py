027e2ab1036490f83006e25ef7652c04a3818852
Philip Strömert <Philip.Stroemert@tib.eu>
2022-03-10 09:20:48 +0100

    Update __init__.py

--- NMRspec/__init__.py
+++ NMRspec/__init__.py
@@ -1,9 +1,7 @@
 __all__ = ["MolecularFormula", "IUPACname", "InCHI", "InCHIKey", "SMILES", "MOLfile", "SolventFormula",
-           "NmrSample", "NmrSolvent", "NmrSolution",
-           "MolarConcentration", "PhValue", "Temperature",
-           "MolarityMeasurementDatum", "PhMeasurementDatum","TemperatureMeasurementDatum",
-           "NmrSpecRecord", "NmrSpecRecordCollection", "MolarityUnit", "Provenance", "PulsedNmrAssay",
-           "Manufacturer"]
+           "NmrSample", "NmrSolvent", "NmrSolution", "MolarConcentration", "PhValue", "Temperature",
+           "MolarityMeasurementDatum", "PhMeasurementDatum","TemperatureMeasurementDatum", "NmrSpecRecord",
+           "NmrSpecRecordCollection", "MolarityUnit", "Provenance", "PulsedNmrAssay", "Manufacturer", "NmrSpectrometer"]
 from NMRspec.NMRspec import *
 from NMRspec.Provenance import *
 
