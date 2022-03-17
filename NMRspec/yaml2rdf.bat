ca6f8c7f553b50c9806023341fe98f030428acf9
Philip Strömert <Philip.Stroemert@tib.eu>
2022-03-17 10:45:50 +0100

    add script for batch ttl conversion

---
+++ NMRspec/yaml2rdf.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+FOR %%f IN (.\jdx_files\output\*.yaml) DO (
+	echo converting %%f to .ttl
+   	linkml-convert -s .\model\schema\NMRspec.yaml -t ttl .\jdx_files\output\%%~nf.yaml -o .\jdx_files\output\%%~nf.ttl
+)
+echo all done
+pause
